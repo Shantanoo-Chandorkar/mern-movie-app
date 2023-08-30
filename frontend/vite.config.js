4cb74443228e707fd451df31929e7faf0901686a
Shantanoo Chandorkar <chandorkarshantanoo@gmail.com>
2023-08-30 19:14:46 +0530

    revert

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://cinemix.onrender.com/",
+        target: "http://localhost:8800",
         changeOrigin: true,
       },
     },
